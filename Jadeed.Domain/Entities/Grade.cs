b9ac87f3293cca28efa2f7d5dc2563dd001d0171
Nozimjon Usmonaliyev <usminaliyevnozimjon@gmail.com>
2023-03-23 18:17:37 +0500

    Create the models

---
+++ Jadeed.Domain/Entities/Grade.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jadeed.Domain.Entities
+{
+    internal class Grade
+    {
+    }
+}
