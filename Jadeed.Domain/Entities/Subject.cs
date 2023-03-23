fc03d4bd52ac4b2692c9419e0f3bf58cd2126eb9
KomronSharpist <komrondeveloper@gmail.com>
2023-03-23 18:07:39 +0500

    Dashboard, Subject and SubjectMark added.

---
+++ Jadeed.Domain/Entities/Subject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jadeed.Data.Entities;
+
+public class Subject
+{
+    public string Name { get; set; }
+    public DateTime StartedTime { get; set; }
+    public DateTime Duration { get; set; }
+    public ICollection<Dashboard> Dashboards { get; set;}
+}
---
+++ Jadeed.Domain/Entities/Subject_mark.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jadeed.Data.Entities;
+
+public class Subject_mark
+{
+    public long StudentDetailId { get; set; }
+    //public StudentDetails StudentDetails { get; set; }
+    //public SubjectType SubjectType { get; set; } 
+    public int Mark { get; set; }
+}
