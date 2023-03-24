bb5f3e61d59c2b1babbe689cfb0cee572a0328cb
Odina Abduganiyeva <125026739+odinaabduganiyeva03@users.noreply.github.com>
2023-03-24 15:33:33 +0300

    Created UserCreationDto, StudentDetailsCreationDto, TeacherDetailsCreationDto

--- Jadeed.Domain/Enums/GenderType.cs
+++ Jadeed.Domain/Enums/GenderType.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Jadeed.Domain.Enums
+﻿namespace Jadeed.Domain.Enums
 {
-    internal class GenderType
+    public enum GenderType
     {
     }
 }
--- Jadeed.Service/DTOs/UserCreationDto.cs
+++ Jadeed.Service/DTOs/UserCreationDto.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using Jadeed.Domain.Enums;
 namespace Jadeed.Service.DTOs
 {
     public class UserCreationDto
     {
         public string Firstname { get; set; }
         public string Lastname { get; set; }
+        public DateTime DateOfBirth { get; set; }
         public UserRole Role { get; set; }
+        public long AddressId { get; set; }
+        public int Phone { get; set; }
+        public GenderType Gender { get; set; }
+        public long StudentDetailsId { get; set; }
+        public long TeacherDetailsId { get; set; }
     }
 }
