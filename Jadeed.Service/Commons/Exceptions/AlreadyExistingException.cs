626d3cf1e155c232077db776277e8031bf7bbb78
Muxammad <muxammadcodes@gmail.com>
2023-03-24 14:04:02 +0500

    PasswordHasher Added

---
+++ Jadeed.Service/Commons/Exceptions/AlreadyExistingException.cs
@@ -0,0 +1,12 @@
+﻿namespace Jadeed.Service.Commons.Exceptions
+{
+    public class AlreadyExistingException : Exception
+    {
+        public string Point { get; set; } = string.Empty;
+        public AlreadyExistingException(string point,string message)
+            :base(message) 
+        {
+            this.Point = point;
+        }
+    }
+}
